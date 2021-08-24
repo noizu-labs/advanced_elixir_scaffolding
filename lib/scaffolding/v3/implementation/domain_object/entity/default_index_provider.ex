1880d417121abae5aabab6e828a25319401170a3
Keith Brings <keith.brings@noizu.com>
2021-08-24 19:48:24 +0700

    Insert and Update index support added. Tweaks needed to restore delete rt record, and to track entites that need to be added to a delta.

--- lib/scaffolding/v3/implementation/domain_object/entity/default_index_provider.ex
+++ lib/scaffolding/v3/implementation/domain_object/entity/default_index_provider.ex
@@ -1,14 +1,37 @@
 defmodule Noizu.ElixirScaffolding.V3.Implementation.DomainObject.Entity.DefaultIndexProvider do
 
   defmodule Default do
-
-    def __write_index__(domain_object, _entity, _index, _settings, _context, _options) do
-      IO.puts "TODO - #{domain_object} - iterate over indexes (if any) and call their insert methods."
+    alias Giza.SphinxQL
+    require Logger
+    def __write_index__(domain_object, entity, index, settings, context, options) do
+      cond do
+        settings[:options][:type] == :real_time ->
+          # @todo tweak header, return raw fields or only field lists not the replace statement.
+          replace = index.__index_header__(:real_time, context, [])
+          fields = index.__index_record__(:real_time, entity, context, []) # todo merge options / settings. E.g. for locale randomization etc.
+          record = Enum.join(fields, " ,")
+          query = replace <> " (" <> record <> ") "
+          Logger.info "SPHINX QUERY| #{query}"
+          SphinxQL.new() |> SphinxQL.raw(query) |> SphinxQL.send()
+        :else ->
+          IO.puts "TODO - #{domain_object} - Perform record keeping so entity's can be reindexed/delta-indexed. etc."
+      end
     end
-    def __update_index__(domain_object, _entity, _index, _settings, _context, _options) do
-      IO.puts "TODO - #{domain_object} - iterate over indexes (if any) and call their update methods."
+    def __update_index__(domain_object, entity, index, settings, context, options) do
+      cond do
+        settings[:options][:type] == :real_time ->
+          replace = index.__index_header__(:real_time, context, [])
+          fields = index.__index_record__(:real_time, entity, context, []) # todo merge options / settings. E.g. for locale randomization etc.
+          record = Enum.join(fields, " ,")
+          query = replace <> " (" <> record <> ") "
+          Logger.info "SPHINX QUERY| #{query}"
+          SphinxQL.new() |> SphinxQL.raw(query) |> SphinxQL.send()
+        :else ->
+          IO.puts "TODO - Perform record keeping so entity's can be reindexed/delta-indexed. etc."
+      end
     end
     def __delete_index__(domain_object, _entity, _index, _settings, _context, _options) do
+      # needed
       IO.puts "TODO - #{domain_object} - iterate over indexes (if any) and call their delete methods."
     end
 
--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Noizu.Scaffolding.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env),
       description: "Noizu Scaffolding",
       docs: docs(),
-      xref: [exclude: [Phoenix.HTML, UUID, XmlBuilder, HtmlSanitizeEx, Ecto.CastError, Redix, Ecto.Type, Plug.Conn, Poison, Poison.Encoder, Noizu.FastGlobal.Cluster]]
+      xref: [exclude: [Phoenix.HTML, UUID, XmlBuilder, HtmlSanitizeEx, Ecto.CastError, Redix, Ecto.Type, Plug.Conn, Poison, Poison.Encoder, Noizu.FastGlobal.Cluster, Giza.SphinxQL]]
     ]
   end # end project
 
