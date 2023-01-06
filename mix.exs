b7b4b2bc08fd1de32b562ddd7a6a4183234bade8
Keith Brings <keith.brings@noizu.com>
2023-01-06 18:58:46 +0700

    Updated ElixirCore dep, FG tweaks

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Noizu.AdvancedScaffolding.Mixfile do
       {:elixir_uuid, "~> 1.2" },
       {:ex_doc, "~> 0.25.1", only: [:test, :dev], optional: true}, # Documentation Provider
       {:markdown, github: "devinus/markdown", optional: true}, # Markdown processor for ex_doc
-      {:noizu_core, github: "noizu/ElixirCore", tag: "1.0.20"},
+      {:noizu_core, github: "noizu/ElixirCore", tag: "1.0.21"},
       {:noizu_mnesia_versioning, github: "noizu/MnesiaVersioning", tag: "0.1.9", override: true},
       {:redix, github: "whatyouhide/redix", tag: "v0.7.0", optional: true},
       {:rocksdb, "~> 1.7", optional: true},
